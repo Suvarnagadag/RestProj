da6635bb78fdcda889b4e2b0a9095b9d15de0a78
Suvarnagadag <91989765+Suvarnagadag@users.noreply.github.com>
2021-10-05 13:54:52 -0700

    Update HelloWord.java
    addded welcome note

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,10 @@
 public class HelloWord{
   public satic void main(String args[]){
-   system.out.println("hello word")
+   system.out.println("hello word");
+       system.out.println("welcome to java");
+           system.out.println("welcome to java world");
+
+    
+
   }
   }
